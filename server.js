90c451584cccce772ce70281320f800a262e9772
Nzaro22 <nzaro4@gmail.com>
2021-10-17 18:25:25 +0300

    added question

--- server.js
+++ server.js
@@ -12,7 +12,53 @@ const server = http.createServer((req, res) => {
             fs.readFile("./html/index.html", "utf8", (err, data) => {
                 if (err) {
                     res.writeHead(404);
-                    res.write(err)
+                    res.write(JSON.stringify(err))
+                }
+                else {
+                    res.write(data)
+                }
+                res.end()
+            })
+            // fs.readFile("./css/index.css", "utf8", (err, data) => {
+            //     if (err) {
+            //         res.writeHead(404, {'Content-Type': 'text/css'});
+            //         res.write(JSON.stringify(err))
+            //     }
+            //     else {
+            //         res.writeHead(404, {'Content-Type': 'text/css'});
+            //         res.write(data)
+            //     }
+            //     res.end()
+            // })
+            // fs.readFile("./js/index.js", "utf8", (err, data) => {
+            //     if (err) {
+            //         res.writeHead(404, {'Content-Type': 'text/javascript'});
+            //         res.write(JSON.stringify(err))
+            //     }
+            //     else {
+            //         res.writeHead(200, {'Content-Type': 'text/javascript'});
+            //         res.write(data)
+            //     }
+            //     res.end()
+            // })
+            break;
+        case "/css/index.css":
+            fs.readFile("./css/index.css", "utf8", (err, data) => {
+                if (err) {
+                    res.writeHead(404);
+                    res.write(JSON.stringify(err))
+                }
+                else {
+                    res.write(data)
+                }
+                res.end()
+            })
+            break;
+        case "/js/index.js":
+            fs.readFile("./js/index.js", "utf8", (err, data) => {
+                if (err) {
+                    res.writeHead(404);
+                    res.write(JSON.stringify(err))
                 }
                 else {
                     res.write(data)
@@ -24,7 +70,31 @@ const server = http.createServer((req, res) => {
             fs.readFile("./html/about.html", "utf8", (err, data) => {
                 if (err) {
                     res.writeHead(404);
-                    res.write(err)
+                    res.write(JSON.stringify(err))
+                }
+                else {
+                    res.write(data)
+                }
+                res.end()
+            })
+            break;
+        case "/css/about.css":
+            fs.readFile("./css/about.css", "utf8", (err, data) => {
+                if (err) {
+                    res.writeHead(404);
+                    res.write(JSON.stringify(err))
+                }
+                else {
+                    res.write(data)
+                }
+                res.end()
+            })
+            break;
+        case "/js/about.js":
+            fs.readFile("./js/about.js", "utf8", (err, data) => {
+                if (err) {
+                    res.writeHead(404);
+                    res.write(JSON.stringify(err))
                 }
                 else {
                     res.write(data)
@@ -44,25 +114,48 @@ const server = http.createServer((req, res) => {
                 res.end()
             })
             break;
+        case "/css/sales.css":
+            fs.readFile("./css/sales.css", "utf8", (err, data) => {
+                if (err) {
+                    res.writeHead(404);
+                    res.write(JSON.stringify(err))
+                }
+                else {
+                    res.write(data)
+                }
+                res.end()
+            })
+            break;
+        case "/js/sales.js":
+            fs.readFile("./js/sales.js", "utf8", (err, data) => {
+                if (err) {
+                    res.writeHead(404);
+                    res.write(JSON.stringify(err))
+                }
+                else {
+                    res.write(data)
+                }
+                res.end()
+            })
+            break;
         case "/products":
-                for (const element of products) {
-                    res.write(`
+            for (const element of products) {
+                res.write(`
                     <P>${element.name}</P>
                     <P>${element.price}</P>
                     <img src="${element.img}" style="height:200px;width:300px">
                     `)
-
-                }
-                res.end()
+            }
+            res.end()
 
             break;
-            case "/product":
-                    res.write(`
+        case "/product":
+            res.write(`
                     <P>${products[2].name}</P>
                     <P>${products[2].price}</P>
                     <img src="${products[2].img}" style="height:200px;width:300px">
                     `)
-                    res.end()
+            res.end()
             break;
 
         default:
